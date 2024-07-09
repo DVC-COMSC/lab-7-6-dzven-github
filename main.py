eaf9fd5aeaa2444c20b87495f7187b6110120672
dzven-github <dzvenigorodsky719@insite.4cd.edu>
2024-07-09 14:36:50 -0700

    Lab is completed (pytest handle is invalid)

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
-
 def getInput():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
+    uservalues = input("Please enter list of values: ")
+    return list(map(int, uservalues.split()))
 
 
 def makeReverse(numbers):
+    result = []
     """
-    ########################################
-    Code Your Program here
-    ########################################
+    # using insert method
+    for elem in numbers:
+        result.insert(0, elem)
     """
+
+    # using pop/append methods
+    while len(numbers) > 0:
+        result.append(numbers.pop())
+
+    return result
 
 
 def main():
     numbers = getInput()
     ret = makeReverse(numbers)
-    print(f'The result values are: {ret}')
+    print(f"The result values are: {ret}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
